276887de8570f202e37815cd573d3be3faa5243b
michjua <michjua@users.noreply.github.com>
2016-06-03 23:53:29 -0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,1 +1,8 @@
-echo "kjdaskdl";
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+               array(
+                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+                   'pdo.username' => $dbopts["user"],
+                   'pdo.password' => $dbopts["pass"]
+               )
+);
